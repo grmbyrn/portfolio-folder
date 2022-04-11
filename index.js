b61b499fb52bb714f1cf7ae6041c99f16f8f2ab3
grmbyrn <graemebyrne_89@hotmail.com>
2022-04-12 00:58:43 +0100

    changed layout

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-const navToggle = document.querySelector('.nav-toggle')
+const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link')
 
 navToggle.addEventListener('click', () => {
-    document.body.classList.toggle('nav-open')
-})
+    document.body.classList.toggle('nav-open');
+});
 
 navLinks.forEach(link => {
     link.addEventListener('click', () => {
-        document.body.classList.remove('nav-open')
+        document.body.classList.remove('nav-open');
     })
 })
-
 
 
 var c = document.getElementById("canvas");
